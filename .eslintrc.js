e0f66f5d102b82cae7fe8ecc8f6ce4e77677d3f1
Dawid Urbas <dawid@getbold.agency>
2024-03-04 16:50:35 +0100

    Upgrade package

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false
+    }
+  }
+}
